82c7218a9f9fe6d2416f7c4e446eca5fbd7918b7
Sarah Wimark <sarahwimark@gmail.com>
2019-06-08 13:55:46 +0200

    Adding routes

--- server.js
+++ server.js
@@ -4,4 +4,10 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
+app.get('/', (req, res) => res.json({ message: 'Welcome to the Contact Keeper API' }))
+
+app.use('/api/users', require('./routes/users'))
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/contacts', require('./routes/contacts'))
+
 app.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}`))
