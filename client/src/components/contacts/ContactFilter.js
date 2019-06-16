22cabbee59f32a0e094c0cd00d6571e742cf47b7
Sarah Wimark <sarahwimark@gmail.com>
2019-06-16 16:58:57 +0200

    Adding edit, delete and filter functionality

---
+++ client/src/components/contacts/ContactFilter.js
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react'
+import ContactContext from '../../context/contact/contactContext'
+
+const ContactFilter = () => {
+  const contactContext = useContext(ContactContext)
+
+  const { } = contactContext
+  return (
+    <form>
+      <h1>1</h1>
+    </form>
+  )
+}
+
+export default ContactFilter
