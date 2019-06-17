d347b2def6d98f8e7d5eb2a786d6645fa8baf0c5
Sarah Wimark <sarahwimark@gmail.com>
2019-06-17 21:15:56 +0200

    Deploying

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const connectDB = require('./config/db')
+const path = require('path')
 
 const app = express()
 
@@ -11,10 +12,19 @@ app.use(express.json({ extended: false }))
 
 const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => res.json({ message: 'Welcome to the Contact Keeper API' }))
-
 app.use('/api/users', require('./routes/users'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/contacts', require('./routes/contacts'))
 
-app.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}`))
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  )
+}
+
+app.listen(PORT, () =>
+  console.log(`Server started on http://localhost:${PORT}`)
+)
